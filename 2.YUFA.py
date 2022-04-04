66a1521d0c9f0b027a04de959d24284530a47bd0
Little Hanhan <79244276+LittleHanhanandBigHanhan@users.noreply.github.com>
2022-04-04 15:44:17 +0800

    ll1

--- 2.YUFA.py
+++ 2.YUFA.py
@@ -1,13 +1,17 @@
-test
-
-path = './test.txt'
+formula_path = './formula.txt'
+token_path = './token.txt'
 dic = {}
+tokens = []
+ll1_stack = []
+
 ffirstSet = {}
 cfirstSet = {}
 followSet = {}
 predictSet = {}
 vt = {'+', '*', 'i', '(',')'}
 S = 'E'
+
+
 
 #生成cfirst集和ffirst集
 def genFirstSet(key):
@@ -60,7 +64,7 @@ def findFirstSet(vl):
 
 #生成follow集
 def genFollowSet():
-
+    maychange = []
     for key in dic.keys():
         for formula in dic[key].split(' | '):
             if formula == '$':
@@ -69,26 +73,41 @@ def genFollowSet():
                 vl = formula.split(' ')
                 for v in range(len(vl)):
                     if vl[v] not in vt:
-                        #这里取交集而不用‘=’，S={’#‘}影响
+                        #这里取交集而不用‘=’
                         followSet[vl[v]] |= findFirstSet(vl[v+1:])
                         if '$' in followSet[vl[v]]:
                             followSet[vl[v]].remove('$')
                             followSet[vl[v]] |= followSet[key]
-                            # 将vl[v]记录下来，这是每次迭代可能发生变化的地方
-
+                            # 将vl[v],key记录下来，这是每次迭代可能发生变化的地方
+                            if vl[v] != key:
+                                maychange.append([vl[v],key])
+    print(maychange)
+    flag = 1
+    while flag == 1:
+        flag = 0
+        for t in maychange:
+            temp = followSet[t[0]]
+            followSet[t[0]] |= followSet[t[1]]
+            if temp != followSet[t[0]]:
+                flag = 1
     return
-
-
 
 #生成predict集
 def genPredictSet():
+    for key in dic.keys():
+        for formula in dic[key].split(' | '):
+
+            predictSet[key + ' = ' + formula] = ffirstSet[key + ' = ' + formula]
+            if '$' in predictSet[key + ' = ' + formula]:
+                predictSet[key + ' = ' + formula].remove('$')
+                predictSet[key + ' = ' + formula] |= followSet[key]
     return
 
 
 
 #主程序
 #读取产生式
-f = open(path, 'r')
+f = open(formula_path, 'r')
 for line in f.readlines():
     temp = line.strip('\n').split(' = ')
     dic[temp[0]] = temp[1]
@@ -96,20 +115,50 @@ for line in f.readlines():
     cfirstSet[temp[0]] = set()
     #follow集初始化
     followSet[temp[0]] = set()
+f.close()
 print(dic)
 
 #生成first集
 for key in dic.keys():
     genFirstSet(key)
+print("first集")
 print(ffirstSet)
 print(cfirstSet)
 
 #生成follow集
 followSet[S].add('#')
 genFollowSet()
+print("folow集")
 print(followSet)
 
-# #生成predict集
-# genPredictSet()
+#生成predict集
+genPredictSet()
+print("predect集")
+print(predictSet)
+
+#LL(1)
+#读取token序列
+ll1_stack.append(S)
+f = open(token_path,'r')
+tokens = f.read().strip().split(' ')
+for token in tokens:
+    while ll1_stack[-1] != token:
+        #flag记录是否匹配成功
+        flag = 0
+        #VN的每一个产生式
+        for formula in dic[ll1_stack[-1]].split(' | '):
+            if token in predictSet[ll1_stack[-1] + ' = ' + formula]:
+                flag = 1
+                ll1_stack.pop()
+                for v in formula.split(' ').reverse():
+                    ll1_stack.append(v)
+                break
+    if flag == 0:
+        #错误处理
+    #弹出匹配上的token，匹配下一个token
+    ll1_stack.pop()
+#匹配成功
+print('success!')
+
 
 
