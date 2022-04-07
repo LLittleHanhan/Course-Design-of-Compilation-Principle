6bc6109352aac5e05646efebcb07a534b520fd7f
akaganeite <jetta_jin@163.com>
2022-04-07 22:22:08 +0800

    update

--- 1.CIFA.py
+++ 1.CIFA.py
@@ -24,7 +24,7 @@ class lex:
              'READ': "READ",'WRITE': "WRITE",
              'RETURN': "RETURN",'INTEGER_T': "INTEGER",
              'CHAR_T': "CHAR",'ID': "ID",
-             'INTC_VAL': "INTC_VAL",'CHARC_VAL': "CHAR_VAL",
+             'int': "INTC",'CHARC_VAL': "CHAR_VAL",
              'ASSIGN': ":=",'EQ': "=",
              'LT': "<",'PLUS': "+",
              'MINUS': "-",'TIMES': "*",
@@ -37,16 +37,45 @@ class lex:
 
     # 程序
     content=''
-
+    line=0
     def __init__(self,content):
         self.content=content
 
     def is_reserved(self,ch):
         return ch in self.reserved_words.keys()
 
-    #def scan(self,ch):
-        #if(ch.isalpha()):
-
+    def scan(self,ch):
+        if ch=='\n': self.line+=1
+        result={}
+        Len = ch.size()
+        i = 0
+        if ch[i].isalpha():                                    #如果字符是字母
+            temp = ch[i]
+            i += 1
+            while ch[i].isalpha() or ch[i].isdigit():          #以该字母起始的字母或数字记录为标识符
+                temp += ch[i]                                  #temp存放标识符
+                if i==Len:
+                    break                                      #结尾退出
+            if self.is_reserved(temp):
+                result['LEX']=self.reserved_words[temp]        #保留字的处理
+                result['SEM']=temp
+                result['LINE']=self.line
+                return result
+            result['LEX'] = 'ID'                               #标识符的处理
+            result['SEM'] = temp
+            result['LINE'] = self.line
+            return result
+        elif ch[i].isdigit():                                  #如果字符是数字
+            temp = ch[i]
+            i += 1
+            while ch[i].isdigit():
+                temp += ch[i]
+                if i == Len:
+                    break
+            result['LEX'] = self.reserved_words['int']         # 数字的处理
+            result['SEM'] = temp
+            result['LINE'] = self.line
+            return result
 
 
 l=lex('a')
