965307149c224b453c9e86f5b6d297f173ae5c39
akaganeite <jetta_jin@163.com>
2022-04-16 21:01:44 +0800

    update

--- app.py
+++ app.py
@@ -1,53 +1,127 @@
-#GUI
-from flexx import flx
-from CIFA import lex
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-class UserInput(flx.Widget):
+"""
+Created on 2018年5月29日
+@author: Irony
+@site: https://pyqt.site , https://github.com/PyQt5
+@email: 892768447@qq.com
+@file: LeftTabWidget
+@description:
+"""
 
-    def init(self):
-        with flx.VBox():
-            self.edit = flx.LineEdit(placeholder_text='Your name')
-            flx.Widget(flex=1)
+from random import randint
 
-    @flx.reaction('edit.user_done')
-    def update_user(self, *events):
+from PyQt5.QtCore import *
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QListWidget, QStackedWidget, QHBoxLayout, \
+        QListWidgetItem, QLabel
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtCore import QUrl
 
-        self.root.store.set_username(self.edit.text)
-        l = lex()
-        a = l.run(self.root.store.username)
+class LeftTabWidget(QWidget):
 
-class SomeInfoWidget(flx.Widget):
+    def __init__(self, *args, **kwargs):
+        super(LeftTabWidget, self).__init__(*args, **kwargs)
+        self.resize(1000, 800)
+        # 左右布局(左边一个QListWidget + 右边QStackedWidget)
+        layout = QHBoxLayout(self, spacing=0)
+        layout.setContentsMargins(0, 0, 0, 0)
+        # 左侧列表
+        self.listWidget = QListWidget(self)
+        layout.addWidget(self.listWidget)
+        # 右侧层叠窗口
+        self.stackedWidget = QStackedWidget(self)
+        layout.addWidget(self.stackedWidget)
+        self.initUi()
 
-    def init(self):
-        with flx.FormLayout():
-            flx.Label(title='program:\n', text=lambda: self.root.store.username)
-            flx.Widget(flex=1)
+    def set_left_list(self,text):
+        item = QListWidgetItem(
+            QIcon('Data/0%d.ico' % randint(1, 8)), str(text), self.listWidget)
+        # 设置item的默认宽高(这里只有高度比较有用)
+        item.setSizeHint(QSize(16777215, 60))
+        # 文字居中
+        item.setTextAlignment(Qt.AlignCenter)
 
-class Store(flx.JsComponent):
+    def initUi(self):
+        # 初始化界面
+        # 通过QListWidget的当前item变化来切换QStackedWidget中的序号
+        self.listWidget.currentRowChanged.connect(
+            self.stackedWidget.setCurrentIndex)
+        # 去掉边框
+        self.listWidget.setFrameShape(QListWidget.NoFrame)
+        # 隐藏滚动条
+        self.listWidget.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.listWidget.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 
-    username = flx.StringProp(settable=True)
+        self.set_left_list('program')
+        self.set_left_list('LEX')
+        self.set_left_list('parse')
+        self.set_left_list('SEM')
+        '''
+        # 这里就用一般的文本配合图标模式了(也可以直接用Icon模式,setViewMode)
+        for i in range(3):
+            item = QListWidgetItem(
+                QIcon('Data/0%d.ico' % randint(1, 8)), str('选 项 %s' % i), self.listWidget)
+            # 设置item的默认宽高(这里只有高度比较有用)
+            item.setSizeHint(QSize(16777215, 60))
+            # 文字居中
+            item.setTextAlignment(Qt.AlignCenter)      
+        '''
 
-class Example(flx.Widget):
+        view = QWebEngineView()
+        view.load(QUrl(QFileInfo("2.YUFA/GrammarTree.html").absoluteFilePath()))
+        self.stackedWidget.addWidget(view)
 
-    store = flx.ComponentProp()
-
-    def init(self):
-
-        # Create our store instance
-        self._mutate_store(Store())
-
-        # Imagine this being a large application with many sub-widgets,
-        # and the UserInput and SomeInfoWidget being used somewhere inside it.
-        with flx.HSplit():
-            with flx.VBox(flex=1):
-                UserInput(flex=1)
-                SomeInfoWidget(flex=10)
-            with flx.TabLayout(flex=3):
-                SomeInfoWidget(title='LEX')
-                flx.Widget(style='background:#eee;')
+        # 再模拟20个右侧的页面(就不和上面一起循环放了)
+        #for i in range(4):
+        #label = QLabel('我是页面 %d' % i, self)
+        #label.setAlignment(Qt.AlignCenter)
+        # 设置label的背景颜色(这里随机)
+        # 这里加了一个margin边距(方便区分QStackedWidget和QLabel的颜色)
+        #label.setStyleSheet('background: rgb(%d, %d, %d);margin: 50px;' % (
+        #randint(0, 255), randint(0, 255), randint(0, 255)))
 
 
-app = flx.App(Example)
-app.launch('app')  # to run as a desktop app
-# app.launch('browser')  # to open in the browser
-flx.run()  # mainloop will exit when the app is closed
+
+# 美化样式表
+Stylesheet = """
+/*去掉item虚线边框*/
+QListWidget, QListView, QTreeWidget, QTreeView {
+    outline: 0px;
+}
+/*设置左侧选项的最小最大宽度,文字颜色和背景颜色*/
+QListWidget {
+    min-width: 120px;
+    max-width: 120px;
+    color: white;
+    background: black;
+}
+/*被选中时的背景颜色和左边框颜色*/
+QListWidget::item:selected {
+    background: rgb(52, 52, 52);
+    border-left: 2px solid rgb(9, 187, 7);
+}
+/*鼠标悬停颜色*/
+HistoryPanel::item:hover {
+    background: rgb(52, 52, 52);
+}
+
+/*右侧的层叠窗口的背景颜色*/
+QStackedWidget {
+    background: rgb(30, 30, 30);
+}
+/*模拟的页面*/
+QLabel {
+    color: white;
+}
+"""
+
+if __name__ == '__main__':
+    import sys
+
+    app = QApplication(sys.argv)
+    app.setStyleSheet(Stylesheet)
+    w = LeftTabWidget()
+    w.show()
+    sys.exit(app.exec_())
