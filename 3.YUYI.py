045228a1fce768f794d2666b882ca6697672b916
swingthehook <513503877@qq.com>
2022-04-17 19:20:55 +0800

    2

--- 3.YUYI.py
+++ 3.YUYI.py
@@ -1,3 +1,48 @@
-import var
+class AtrributeIR:
+    def __init__(self):
+        self.idtype = None
+        self.kind = None
+        self.moreinfo = None
 
-print(var.gramTree)
+class varIR:
+    def __init__(self):
+        self.access = None
+        self.level = None
+        self.off = None
+
+class procIR:
+    def __init__(self):
+        self.level = None
+        self.pram = []
+        self.code = None    #目标代码地址
+        self.size = None
+
+class standardType:
+    def __init__(self):
+        self.size = 1
+        self.kind = None
+
+class arrType:
+    def __init__(self):
+        self.size = None
+        self.kind = "arrayTy"
+        self.indexTy= None
+        self.elemTy = None
+
+class recType:
+    def __init__(self):
+        self.size = None
+        self.kind = "recordTy"
+        self.body
+
+class fieldChain:
+    def __init__(self):
+        self.idname
+        self.unitType
+        self.offset
+        self.next
+
+class typeIR:
+    def __init__(self):
+        self.size
+        self.
