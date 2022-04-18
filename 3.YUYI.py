775f69edf12f8f81b4aff0beb5567aee7ba9d0f0
swingthehook <513503877@qq.com>
2022-04-18 16:35:57 +0800

    2

--- 3.YUYI.py
+++ 3.YUYI.py
@@ -1,11 +1,14 @@
 from classes import *
 import grammar
 import var
+from treelib import node
+grammar.check_grammar()
+#print(var.gramTree.children(0)[0].data.tokenInfo)
 
 
 def analyze():
     initialize()
-    travelsal(var.gramTree)
+    travelsal(var.gramTree.get_node(0))
 
 def initialize():
     intType = standardType("intType")
@@ -13,8 +16,10 @@ def initialize():
     boolType = standardType("boolType")
 
 def travelsal(root):
-    if(root.data !=None):
-        print(1)
-    temp = root.children(root.identifier)
+    if(root.data.tokenInfo !=None):
+        print(root.data.tokenInfo["lex"])
+    temp = var.gramTree.children(root._identifier)
     for i in temp:
         travelsal(i)
+
+analyze()
